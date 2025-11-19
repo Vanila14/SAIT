e96cfa6fe097074959f77f15005deaf6519c81d4
Vanila14 <139687493+Vanila14@users.noreply.github.com>
2025-11-19 14:05:41 +0300

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -48,10 +48,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Ждем 2 секунды (длина клика) перед переходом
             setTimeout(() => {
-                window.location.href = './index.html';
+                window.location.href = '../index.html';
             }, 2000);
         });
     }
 });
+
 
 
