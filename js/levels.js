cf98792ac861da3d3058cc3d4f2fc9a6211348e6
Vanila14 <139687493+Vanila14@users.noreply.github.com>
2025-11-19 14:13:30 +0300

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -48,11 +48,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Ждем 2 секунды (длина клика) перед переходом
             setTimeout(() => {
-                window.location.href = 'https://vanila14.github.io/SAIT/';
+                window.location.href = '/SAIT/index.html';
             }, 2000);
         });
     }
 });
+
 
 
 
